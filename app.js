fbc2bfb7a02e5385fdd80d704b678b7bd47d8223
tonmoy <tonmoy3673@gmail.com>
2025-03-09 21:26:20 +0600

    :zap: implemented toggle password view

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 console.log('connected');
+// =========== get view icon ======//
+const viewIcon = document.getElementById('icon');
 
+// ========== get copy icon =========//
+const copyIcon = document.getElementById('copy');
 // ========== get password value =========//
+const input = document.getElementById('pass')
+input.addEventListener('input',function(){
+  if (input.value) {
+      console.log(input.value);
+    viewIcon.style.display='block'
+    copyIcon.style.display='block'
+
+  }
+  else{
+    viewIcon.style.display='none';
+    copyIcon.style.display='none';
+  }
+  
+})
+
+
+// ============= toggle view icon ======//
+const toggleViewIcon =()=>{
+    if (input.type ==='password') {
+        input.type ='text'
+        viewIcon.src='/images/eye-open.png';
+        
+    }
+   else{
+    input.type='password';
+    viewIcon.src='./images/eye-close.png'
+   }
+}
+
+// =========== copy =========//
